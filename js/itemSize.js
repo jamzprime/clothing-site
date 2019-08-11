bbe4e75f074c57621e790d82ef914a09f351631c
James Prime <37202302+jamzprime@users.noreply.github.com>
2019-08-11 22:18:18 +1200

    Add files via upload

---
+++ js/itemSize.js
@@ -0,0 +1,12 @@
+// Event listeners for displaying selected size of item
+document.getElementById("buttonSizeS").addEventListener("click", function() {
+    document.getElementById("selected-size").innerHTML="S";
+}, false);
+
+document.getElementById("buttonSizeM").addEventListener("click", function() {
+    document.getElementById("selected-size").innerHTML="M";
+}, false);
+
+document.getElementById("buttonSizeL").addEventListener("click", function() {
+    document.getElementById("selected-size").innerHTML="L";
+}, false);
